28edb1e91af5d295eada30e6e64e97dd229600a8
TamasH <unconfigured@null.spigotmc.org>
2022-12-05 11:58:30 +0100

    WhileLus.php aangemaakt met een while lus.

---
+++ WhileLus.php
@@ -0,0 +1,15 @@
+<?php
+
+$teller = 1;
+$maximum = 10;
+
+echo "de teller is voor de while lus: ".$teller."\n";
+
+while ($teller <= $maximum) {
+    echo "Teller is nu: ".$teller."\n";
+    $teller++;
+}
+
+echo "teller is na de while lus: ".$teller."\n";
+
+?>
