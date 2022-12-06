4fedab89432f759e7c4701e4c92539698ec416f1
TamasH <unconfigured@null.spigotmc.org>
2022-12-06 14:32:03 +0100

    Check voor verkeerde invoer van getal.

--- Quiz.php
+++ Quiz.php
@@ -3,33 +3,46 @@
 $i = 0;
 $beurt = 0;
 $random = rand(1, 100);
-$stop = (string)readline("typ [ja] om te stoppen en [enter]om te starten met de quiz!\n");
-$invoer = readline("Raad een getal tussen de 1 & 100\n");
+
+$stop = (string)readline("typ [stop] om te stoppen en [enter]om te starten met de quiz!\n");
+if ($stop == "stop" or $stop == "Stop") {
+    exit("Je hebt de quiz gestopt!");
+}
+
+$invoer = readline("De quiz is gestart! Raad een getal tussen de 1 & 100\n");
 
 while ($i<1) {
 
+    //Als je de invoer goed is gegokt.
     if ($invoer == $random) {
-        echo "Het antwoord is goed!";
-        echo "Je deed er: " . $beurt." beurten over!";
+        echo "Het antwoord is goed!\n\n";
+        echo "Je deed er: " .$beurt." beurt/beurten over!\n\n";
         $i = 1;
+    } 
 
-        if ($stop == "ja" or $stop == "Ja") {
-            $i = 1;
-        }
+    //Een check voor als je een te groot of te klein getal invult.
+    if ($invoer > 100 or $invoer < 0) {
+        exit("De quiz is gestopt! Je kan niet een getal invullen die hoger is dan 100 of lager dan 0!\n\n");
+    }
 
-    } else if ($invoer != $random) {
-        echo "Het is helaas niet goed!\n";
+    //Als de invoer niet goed gegokt is.
+    if ($invoer != $random) {
+        echo "Het is helaas niet goed!\n\n";
         $beurt++;
+
+        //De twee if statements om te checken welke hint gegeven moet worden.
         if ($invoer > $random) {
-            echo "Het getal is lager!\n";
-            $invoer = readline("Raad nog eens een getal tussen de 1 & 100\n");
-        }
+            echo "Het getal is lager!\n\n";
+            $invoer = readline("Raad nog eens een getal tussen de 1 & 100\n\n");
+            
+        } 
         if ($invoer < $random) {
-            echo "Het getal is hoger!\n";
-            $invoer = readline("Raad nog eens een getal tussen de 1 & 100\n");
+            echo "Het getal is hoger!\n\n";
+            $invoer = readline("Raad nog eens een getal tussen de 1 & 100\n\n");
         }
-        $stop = (string)readline("typ [ja] om te stoppen, [enter] om door te gaan.\n");
-        if ($stop == "ja" or $stop == "Ja") {
+
+        $stop = (string)readline("typ [Stop] om te stoppen, [enter] om door te gaan.\n\n");
+        if ($stop == "stop" or $stop == "Stop") {
             $i = 1;
         }
         
