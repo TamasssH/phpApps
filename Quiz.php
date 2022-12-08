5635ad3e579e8e3d7a4aa16ca37ec8396271ed58
TamasH <unconfigured@null.spigotmc.org>
2022-12-08 08:56:00 +0100

    Quiz.php met colorcodes

--- Quiz.php
+++ Quiz.php
@@ -17,7 +17,6 @@ $invoer = readline("\n");
 
 //De loop zodat je opnieuw kunt gokken.
 while ($i<1) {
-
     //Als je de invoer goed is gegokt.
     if ($invoer == $random) {
         echo "\e[0;32mHet antwoord is goed!\e[0m\n";
@@ -26,11 +25,13 @@ while ($i<1) {
     } 
     //Een keuze om te stoppen.
     if ($stop == "stop" or $stop == "Stop") {
-        echo "\e[01;31mDe quiz is gestopt!\e[0m"; exit("\n");
+        echo "\e[01;31mDe quiz is gestopt!\e[0m"; 
+        exit("\n");
     }
     //Een check voor als je een te groot of te klein getal invult.
     if ($invoer > 100 or $invoer < 1)  {
-        echo "\e[01;31mDe quiz is gestopt! Je hebt een getal kleiner dan 1 of groter dan 100 ingevuld!\e[0m"; exit("\n");
+        echo "\e[01;31mDe quiz is gestopt! Je hebt een getal kleiner dan 1 of groter dan 100 ingevuld!\e[0m"; 
+        exit("\n");
     }
     //Als de invoer niet goed gegokt is.
     //De twee if statements om te checken welke hint gegeven moet worden.
@@ -38,8 +39,7 @@ while ($i<1) {
         $beurt++;
         echo "\e[01;31mHet is helaas niet goed! Het getal is lager!\e[0m \n";
         echo "\e[0;33mRaad nog eens een getal tussen de 1 & 100\e[0m";
-        $invoer = readline("\n");
-            
+        $invoer = readline("\n");       
     } 
     if ($invoer < $random) {
         $beurt++;
